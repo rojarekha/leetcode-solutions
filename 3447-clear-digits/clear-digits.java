5a34798221504d691bc94328e30b9994f1abc46c
rojarekha <arugolanurojarekha25@gmail.com>
2026-05-15 14:18:45 +0530

    Time: 3 ms (47.79%) | Memory: 44.2 MB (15.81%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String clearDigits(String s) {
+       Stack <Character> st = new Stack<>();
+       StringBuilder sb=new StringBuilder();
+       for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(ch>='0' && ch<='9'){
+            if(!st.isEmpty()){
+               st.pop();
+            }
+            
+        }else{
+            st.push(ch);
+        }
+    }
+     while(!st.isEmpty()){
+        sb.append(st.pop());
+    }
+    sb.reverse();
+    return sb.toString();
+
+    }
+}
